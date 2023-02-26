c0b1b0eed83c915b8371c5cd5cb4587169d879d5
elf <brave.rabbit@gmail.com>
2023-02-27 00:39:24 +0300

    count

--- Examples/Mentoring/return.py
+++ Examples/Mentoring/return.py
@@ -1,8 +1,8 @@
 def rec(valve):
     print(valve)
-    if valve < 4000:
-        rec(valve+100)
-    print(valve)
+    if valve < 8:
+        rec(valve+1)
+ #   print(valve)
 rec(1)
 #глубина рекрсии 997
 #Рекурсия- функция, которая вызыват сама себя
