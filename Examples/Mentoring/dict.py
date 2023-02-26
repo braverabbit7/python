ec179ed8570649bba8568b7d8b1e439f13df4112
elf <brave.rabbit@gmail.com>
2023-02-26 19:15:31 +0300

    ex7

---
+++ Examples/Mentoring/dict.py
@@ -0,0 +1,8 @@
+#dict - словарь можем обращаться по имени к элементу
+country = {4: 3}
+#ключ: значение
+print(country[4])
+
+country = {"code": "Ru", "name": "Russian", "population": 144}
+#Обращаемся по ключу с именем name
+print(country["name"])
