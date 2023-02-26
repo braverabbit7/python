08b0ad1ca0660751943a02bb9a7fb2294593c40e
elf <brave.rabbit@gmail.com>
2023-02-26 19:18:00 +0300

    ex7

--- Examples/Mentoring/dict.py
+++ Examples/Mentoring/dict.py
@@ -6,3 +6,6 @@ print(country[4])
 country = {"code": "Ru", "name": "Russian", "population": 144}
 #Обращаемся по ключу с именем name
 print(country["name"])
+#используем dict
+country2 = dict(code="ru", name="Russian", )
+print(country2["code"])
