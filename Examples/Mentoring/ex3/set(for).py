eed3353b2fbfbbdb6241021e356d4cb117bc91e5
Semen_Adaikin <brave.rabbit@gmail.com>
2023-03-01 18:43:09 +0300

    set done

--- Examples/Mentoring/ex3/set(for).py
+++ Examples/Mentoring/ex3/set(for).py
@@ -1,20 +1,6 @@
 s1 = set([1, 2, 3, 4])
 s2 = set([3, 4, 5, 6, 6])
-c = []
-for i in s1:
-    for j in s2:
-        if i == j:
-            c.append(i)
-            break
 
-print(c)
-
-for i in s1:
-    if i in c:
-        continue
-    for j in s2:
-        if i == j:
-            c.append(i)
-            break
-
-print(c)
+print("Разница:", s1^s2)
+print("Совпадение:", s1&s2)
+print("Обьединение:",s1|s2)
