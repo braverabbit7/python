fb8086b8dedc8b924ece6da44ea2325679c75156
Semen_Adaikin <brave.rabbit@gmail.com>
2023-03-02 13:53:03 +0300

    set done

---
+++ Examples/Mentoring/ex2/decorators.py
@@ -0,0 +1,40 @@
+#Посчитаем время выполнения функции :
+from datetime import datetime
+def f():
+    stat = datetime.now()
+    l = []
+    for i in range(20000):
+        if i % 2 == 0:
+            l.append(i)
+    print(datetime.now() -stat)
+    return l
+l1 = f()
+
+print(l1)
+
+#Написание Декоратора
+
+def timeit(func):
+    def wrapper():
+        start =datetime.now()
+        result =func()
+        print(datetime.now() - start)
+        return result
+    return wrapper
+
+
+print("С декоратором")
+@timeit
+def f():
+#    stat = datetime.now()
+    l = []
+    for i in range(20000):
+        if i % 2 == 0:
+            l.append(i)
+#    print(datetime.now() -stat)
+    return l
+@timeit
+
+l1 = f()
+
+print(l1)
