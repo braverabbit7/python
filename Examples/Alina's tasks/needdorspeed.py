5cbb036a9fe0266f97fb073d0ca5643590153951
Semen_Adaikin <brave.rabbit@gmail.com>
2023-03-02 20:23:10 +0300

    set done

--- Examples/Alina's tasks/needdorspeed.py
+++ Examples/Alina's tasks/needdorspeed.py
@@ -1,4 +1,7 @@
-i = [1, 2, 3, 4]
-ia = i[0]
-members ={"Ferrafi": ia, "Porche": ia, "Ford": ia}
+cars = {"Ferrari", "Porche", "Ford"}
+num = {1, 7, 54}
+carsl = (list(cars))
+numl = (list(num))
+members ={"Mike": carsl[0], "John": carsl[1], "Diego": carsl[2]}
 print(members)
+#print(list(cars)[1])
