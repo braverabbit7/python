e3546cccfb0bd27bf74498a7c1661499bd2c4f35
Semen_Adaikin <brave.rabbit@gmail.com>
2023-03-06 14:25:04 +0300

    set done

--- Examples/Mentoring/ex2/lambda.py
+++ Examples/Mentoring/ex2/lambda.py
@@ -4,3 +4,17 @@
 # В свою очередь, ключевое слово  lambda  используется для определения анонимной функции.
 double = lambda x: x*2
 print(double(5))
+
+
+
+#____________________
+
+
+
+
+def mul(f, i=2):
+
+    return f(1, 2) * i
+
+print(mul(lambda a, b: a + b))
+
