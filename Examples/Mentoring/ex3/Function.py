542cdf36f16291ef568a84dd1d8b814c9fb0e887
Semen_Adaikin <brave.rabbit@gmail.com>
2023-03-06 13:38:42 +0300

    set done

--- Examples/Mentoring/ex3/Function.py
+++ Examples/Mentoring/ex3/Function.py
@@ -1,4 +1,38 @@
 #Есть встроеные , а можно создовать свои def название_функции(передаваемый параметр)
-def hello():
+def test_func():
 #Передаем что делает функция
-    pass
+    print("Hello", end="")
+    print("!")
+#Вызываем саму функцию
+test_func()
+
+
+print("Передаем значение в функцию")
+
+def test_func2(w):
+    print(w, end="")
+    print("!")
+
+
+test_func2("Hi")
+test_func2(7)
+test_func2(7.6)
+
+print("Еще пример--------------------------(складывание строк и чисел)")
+def sum(a, b):
+    res = a + b
+    print("Result:", res)
+a1 = int(input("Введите число a:"))
+a2 = int(input("Введите число b:"))
+sum(a1, a2)
+b1 = str(a1)
+b2 = str(a2)
+sum(b1, b2)
+
+print("Если надо вернуть знвчение функции return(Вщзвращаем res))")
+
+def sum2(a, b):
+    res = a + b
+    return res
+res = sum2(a1, a2)
+print(res)
