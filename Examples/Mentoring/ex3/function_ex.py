3b6126277b73812d681601d9dcdd1eaceb706e9c
Semen_Adaikin <brave.rabbit@gmail.com>
2023-03-06 13:54:29 +0300

    set done

--- Examples/Mentoring/ex3/function_ex.py
+++ Examples/Mentoring/ex3/function_ex.py
@@ -20,3 +20,24 @@ def minimal(l):
 minimal(nums1)
 minimal(nums2)
 
+print("С функцией return:")
+
+print("Функция для поиска минимального числа:")
+def minimal(l):
+    min_num = l[0]
+    for el in l:
+        if el < min_num:
+            min_num = el
+    return min_num
+min1 = minimal(nums1)
+min2 = minimal(nums2)
+
+if min1<min2:
+    print(min1)
+else:
+    print(min2)
+
+
+
+
+
