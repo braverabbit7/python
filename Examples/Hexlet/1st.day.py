92295e820b5e7b33de323f89088cec0bfd11ae72
Semen_Adaikin <brave.rabbit@gmail.com>
2023-03-07 16:12:35 +0300

    set done

---
+++ Examples/Hexlet/1st.day.py
@@ -0,0 +1,9 @@
+import re
+def reader(filename):
+    regexp = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
+    with open(filename) as f:
+        log = f.read()
+        ips_list = re.findall(regexp, log)
+    print(ips_list)
+if __name__ == '__main__':
+    reader('nginx_logs.txt')
