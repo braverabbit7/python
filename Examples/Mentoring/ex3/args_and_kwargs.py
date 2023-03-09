a9eb6a11d5b3889f28c7be72fc76fa4b2952ed17
Semen_Adaikin <brave.rabbit@gmail.com>
2023-03-09 16:12:43 +0300

    set done

--- Examples/Mentoring/ex3/args_and_kwargs.py
+++ Examples/Mentoring/ex3/args_and_kwargs.py
@@ -2,4 +2,31 @@
 def func(*args):
     print(args)
 
-func(3, 5, 6, "Some text")
+func(3, 5, 6)
+
+
+
+print("Чтобы сделать кортеджем сложение добавляем в func2(*l) * для перевода из котреджа")
+
+def func2(*args):
+    print(args)
+    print(sum(args))
+l = [1, 2, 3]
+func2(*l)
+
+print("Добавим позиионный аргумент a в def func3(a, *args). Первый элемент попадает в а =1:")
+
+def func3(a, *args):
+    print(a)
+    print(args)
+    print(sum(args))
+l = [1, 2, 3]
+func3(*l)
+
+print("**Kwargs ** -означает упаковку в словарь kwargs - key world agreements")
+
+def func4(*args, **kwargs):
+    print(args)
+    print(kwargs)
+l = [1, 2, 3]
+func4(l)
