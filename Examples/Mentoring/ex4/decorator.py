7cb2364778bc817e3dc73328108610745396226c
elf <brave.rabbit@gmail.com>
2023-03-13 01:31:56 +0300

    count

--- Examples/Mentoring/ex4/decorator.py
+++ Examples/Mentoring/ex4/decorator.py
@@ -1,9 +1,26 @@
-from functools import lru_cache
+a = int(input("Введите значение a:"))
+b = int(input("Введите значение b:"))
+database = []
+i = 0
+def decorator(func):
+    def wrapper():
 
 
-def fib(x):
-    d = fib(x-1) + fib(x-2) if x > 1 else x
-    return d
+        print("Код до выполнения:")
+        database.extend([a, b])
+        print(database)
+        print("Код после выполнения:")
+        func()
+        print(i)
 
-print(fib(11))
+    return wrapper
 
+@decorator
+def show():
+
+    i = (a + b)
+
+show()
+#Можно вместо @decorator использовать так
+#dec = decorator (show)
+#dec()
