b2c9f88a5b52d36a1ad6f5a954d67f2139cec670
Semen_Adaikin <brave.rabbit@gmail.com>
2023-03-21 13:49:21 +0300

    decorator

--- Examples/Mentoring/ex4/decorator_with _arguments.py
+++ Examples/Mentoring/ex4/decorator_with _arguments.py
@@ -1,5 +1,18 @@
 #Декораторы функций с параметрами
-def func_decorator(func):
-    def wrapper(x, *args, **kwargs):
-        dx = 0.0001
-        res = ()
+import time
+
+def lim_requests(seconds):
+#Простой декоратор со sleep (обернем сверху еще , чтобы передавать время)
+    def lim_requests_decorator(func):
+        def wrapper(*args, **kwargs):
+            res = func(*args,**kwargs)
+            time.sleep(seconds)
+            return res
+        return wrapper
+    return lim_requests_decorator
+#Функцияя возврвщающая Hi there
+@lim_requests(2)
+def simple():
+    return "Hi there"
+
+print(simple())
