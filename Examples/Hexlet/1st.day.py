18cd79f5bad7876a798bc9abe505f1ebf0528eb5
Semen_Adaikin <brave.rabbit@gmail.com>
2023-03-22 12:30:07 +0300

    decorator

--- Examples/Hexlet/1st.day.py
+++
@@ -1,9 +0,0 @@
-import re
-def reader(filename):
-    regexp = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
-    with open(filename) as f:
-        log = f.read()
-        ips_list = re.findall(regexp, log)
-    print(ips_list)
-if __name__ == '__main__':
-    reader('1.txt')
