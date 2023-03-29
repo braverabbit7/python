e77a4cbc0237bb4328ff8f41a1b0ac119409a0f1
elf <brave.rabbit@gmail.com>
2023-03-29 22:58:42 +0300

    count

--- Examples/Mentoring/ex4/123.py
+++ Examples/Mentoring/ex4/123.py
@@ -1,10 +1,18 @@
+print("For")
 array = [1, 2, 3, 4, 5]
 for i in range(len(array)):
-
     array[i] = array[i]+1
-
 print(array)
 
+print("While")
+array = [1, 2, 3, 4, 5]
+l = 0
+while l < len(array):
+   array[l] = array[l]+1
+   l += 1
+print(array)
 
-
-
+print("List comprehension")
+array = [1, 2, 3, 4, 5]
+array = [a+1 for a in array]
+print(array)
