2933353c2830e7d8e1e8d23e6855d89d50e5b246
Semen_Adaikin <brave.rabbit@gmail.com>
2023-03-30 13:49:05 +0300

    decorator

---
+++ Examples/Mentoring/ex4/decor_hw.py
@@ -0,0 +1,12 @@
+def func_decoraator(func):
+    def wrapper():
+        print("--Before--")
+        func()
+        print("--After--")
+    return wrapper()
+
+def some_func():
+    print("Some_func")
+func_decoraator(some_func)
+
+
